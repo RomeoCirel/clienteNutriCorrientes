ecd564e290347a6ed289043e2bfe60bfc128ee30
Cirel H. Romeo <cirelhromeo@gmail.com>
2021-11-15 08:05:36 -0300

    Instala y configura Tailwaind CSS

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+<div class="bg-yellow-500 font-bold">
   <router-view />
+</div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
-
+import 'tailwindcss/tailwind.css'
 export default defineComponent({
   name: 'App'
 })
