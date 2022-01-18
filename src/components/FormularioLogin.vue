4414f5ee2f121d28ec7eb55696bcb96f874b1216
Cirel H. Romeo <cirelhromeo@gmail.com>
2022-01-18 08:54:28 -0300

    rework de FormularioLogin con useAuth

--- src/components/FormularioLogin.vue
+++ src/components/FormularioLogin.vue
@@ -1,36 +1,9 @@
 <template>
   <div
-    class="
-      mt-10
-      min-h-full
-      w-full
-      sm:w-8/12
-      lg:w-5/12
-      xl:w-4/12
-      flex
-      items-center
-      justify-center
-      py-12
-      sm:py-4
-      px-0
-      sm:px-1
-      lg:px-8
-    "
+    class="mt-0 min-h-full w-full h-full sm:w-8/12 lg:w-5/12 xl:w-4/12 flex items-center justify-center py-12 sm:py-4 px-0 sm:px-1 lg:px-8"
   >
     <div
-      class="
-        max-w-md
-        w-full
-        space-y-8
-        px-4
-        py-5
-        bg-white
-        space-y-6
-        sm:p-6
-        rounded-lg
-        shadow
-        sm:rounded-lg sm:overflow-hidden
-      "
+      class="max-w-md w-full space-y-8 px-4 py-5 bg-white space-y-6 sm:p-6 rounded-lg shadow sm:rounded-lg sm:overflow-hidden"
     >
       <div>
         <div class="flex items-center justify-center">
@@ -41,66 +14,34 @@
           Si no tiene cuenta comunicate con RRHH
         </p>
       </div>
-      <form class="mt-8 space-y-6" action="#" method="POST">
+      <form class="mt-8 space-y-6">
         <input type="hidden" name="remember" value="true" />
         <div class="">
           <div>
             <div>
-              <label for="email-address" class="sr-only">Email address</label>
+              <label for="dni" class="sr-only">DNI</label>
               <input
-                id="email-address"
-                name="email"
-                type="email"
-                autocomplete="email"
+                id="dni"
+                v-model="credenciales.dni"
+                name="dni"
+                type="number"
+                autocomplete="dni"
                 required=""
-                class="
-                  appearance-none
-                  rounded-none
-                  relative
-                  block
-                  w-full
-                  px-3
-                  py-2
-                  border border-gray-300
-                  placeholder-gray-500
-                  text-gray-900
-                  rounded-t-md
-                  focus:outline-none
-                  focus:ring-indigo-500
-                  focus:border-indigo-500
-                  focus:z-10
-                  sm:text-sm
-                "
-                placeholder="Email address"
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="D.N.I."
               />
             </div>
             <div>
-              <label for="password" class="sr-only">Password</label>
+              <label for="password" class="sr-only">Contraseña</label>
               <input
+                v-model="credenciales.password"
                 id="password"
                 name="password"
                 type="password"
                 autocomplete="current-password"
                 required=""
-                class="
-                  appearance-none
-                  rounded-none
-                  relative
-                  block
-                  w-full
-                  px-3
-                  py-2
-                  border border-gray-300
-                  placeholder-gray-500
-                  text-gray-900
-                  rounded-b-md
-                  focus:outline-none
-                  focus:ring-indigo-500
-                  focus:border-indigo-500
-                  focus:z-10
-                  sm:text-sm
-                "
-                placeholder="Password"
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Contraseña"
               />
             </div>
           </div>
@@ -112,14 +53,7 @@
               id="remember-me"
               name="remember-me"
               type="checkbox"
-              class="
-                h-4
-                w-4
-                text-indigo-600
-                focus:ring-indigo-500
-                border-gray-300
-                rounded
-              "
+              class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
             />
             <label for="remember-me" class="ml-2 block text-sm text-gray-900">
               Recuerdame
@@ -137,27 +71,9 @@
 
         <div>
           <button
-            type="submit"
-            class="
-              group
-              relative
-              w-full
-              flex
-              justify-center
-              py-2
-              px-4
-              border border-transparent
-              text-sm
-              font-medium
-              rounded-md
-              text-white
-              bg-indigo-600
-              hover:bg-indigo-700
-              focus:outline-none
-              focus:ring-2
-              focus:ring-offset-2
-              focus:ring-indigo-500
-            "
+            @click="iniciarSesion"
+            type="button"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
               <LockClosedIcon
@@ -174,15 +90,31 @@
 </template>
 
 <script lang="ts">
-// import { ref } from 'vue'
+import { defineComponent } from 'vue'
+import useAuth from 'src/composables/useAuth'
 import { LockClosedIcon } from '@heroicons/vue/solid'
 import IsoLogo from '../components/IsoLogo.vue'
-
-export default {
+export default defineComponent({
   name: 'FormularioLogin',
   components: {
     LockClosedIcon,
     IsoLogo
+  },
+  emits: ['irHome'],
+  setup(props, { emit }) {
+    const { loguear, credenciales, estaLogueado } = useAuth()
+    return {
+      iniciarSesion: async (): Promise<void> => {
+        const res = await loguear(credenciales)
+        if (res.ok) {
+          if (res.estado === 200) {
+            void emit('irHome')
+          }
+        }
+      },
+      credenciales,
+      estaLogueado
+    }
   }
-}
+})
 </script>
