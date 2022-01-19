f98955a989c7b8730c2cb6952622d838007480d7
Cirel H. Romeo <cirelhromeo@gmail.com>
2022-01-19 00:58:11 -0300

    Agrega la recuperacion de sesion al cargar App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="h-full bg-gray-100 text-gray-600">
+  <div class="h-full h-screen bg-gray-100 text-gray-600">
     <router-view />
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import useAuth from 'src/composables/useAuth'
+import { useRouter } from 'vue-router'
 import 'tailwindcss/tailwind.css'
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  setup() {
+    const { estaLogueado, recuperarSession } = useAuth()
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    window.enrutador = useRouter()
+    void recuperarSession()
+    return {
+      estaLogueado
+    }
+  }
 })
 </script>
