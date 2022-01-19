4f97765fcd81a804cade667af5210438bbd58adf
Cirel H. Romeo <cirelhromeo@gmail.com>
2022-01-19 00:47:29 -0300

    login con composable useAuth

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,23 @@
 <template>
-  <q-page class="row items-start justify-evenly">
-    <FormularioLogin />
-  </q-page>
+  <div class="row items-start justify-evenly h-full">
+    <FormularioLogin @irHome="irHome" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import FormularioLogin from '../components/FormularioLogin.vue'
+import FormularioLogin from 'src/components/FormularioLogin.vue'
+import { useRouter } from 'vue-router'
+import { rutaHome } from 'src/router/rutasNombres'
 
 export default defineComponent({
-  components: { FormularioLogin }
+  name: 'login',
+  components: { FormularioLogin },
+  setup() {
+    const router = useRouter()
+    return {
+      irHome: () => void router.push({ name: rutaHome })
+    }
+  }
 })
 </script>
